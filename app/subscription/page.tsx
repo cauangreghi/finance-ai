a717c61a01270d942d2520cc597123efe222364a
Cauan <cauangregui@outlook.com>
2025-01-09 10:46:16 -0300

    fix: on subscription page named getCurrentDate import variable

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
 import { Badge } from "../_components/ui/badge";
-import { getCurrentMonthTransactions } from "../_data/get-current-month-transactions";
+import { getCurrentDateTransactions } from "../_data/get-current-month-transactions";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -13,7 +13,7 @@ const SubscriptionPage = async () => {
     redirect("/login");
   }
   const user = await clerkClient().users.getUser(userId);
-  const currentMonthTransactions = await getCurrentMonthTransactions();
+  const currentMonthTransactions = await getCurrentDateTransactions();
   const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
